d87f2493265799161f033bb1d48b861b401ed0b8
mr56xcpm <anonymoussabe4@gmail.com>
2022-02-24 22:56:22 -0600

    test

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,27 +1,36 @@
-import React from "react";
-import ai from "../../assets/ai.png";
-import "./header.css";
+import React from 'react';
+import ai from '../../assets/ai.png';
+import './header.css';
 
-const Header = () => (
-  <div className='gpt3__header section__padding' id='home'>
-    <div className='gpt3__header-content'>
-      <h1 className='gradient__text'>Inteligencia Artifical</h1>
-      <p>
-        la inteligencia artificial (AI) es una herramienta que hace posible que
-        las máquinas aprendan de la experiencia, se ajusten a distintos cambios
-        y realicen tareas similares a las humanas. La AI se está utilizando en
-        diferentes industrias, incluidas las finanzas y la atención médica.
-      </p>
+const Header = () => {
+  const openLink = () => {
+    const url = '#wgpt3';
+    window.scroll(url);
+  };
 
-      <div className='gpt3__header-content__input'>
-        <button type='button'>Conoce Más</button>
+  return (
+    <div className='gpt3__header section__padding' id='home'>
+      <div className='gpt3__header-content'>
+        <h1 className='gradient__text'>Inteligencia Artifical</h1>
+        <p>
+          la inteligencia artificial (AI) es una herramienta que hace posible
+          que las máquinas aprendan de la experiencia, se ajusten a distintos
+          cambios y realicen tareas similares a las humanas. La AI se está
+          utilizando en diferentes industrias, incluidas las finanzas y la
+          atención médica.
+        </p>
+
+        <div className='gpt3__header-content__input'>
+          <button type='button' onClick={() => openLink()}>
+            Conoce Más
+          </button>
+        </div>
+      </div>
+
+      <div className='gpt3__header-image'>
+        <img src={ai} />
       </div>
     </div>
-
-    <div className='gpt3__header-image'>
-      <img src={ai} />
-    </div>
-  </div>
-);
-
+  );
+};
 export default Header;
